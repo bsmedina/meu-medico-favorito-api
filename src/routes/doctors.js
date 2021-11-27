d00f4fed0d3b35d2b6105a3752798c491f128d5a
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-26 22:05:17 -0300

    rotas put e get

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const controller = require("../controllers/doctorsController.js");
 
 router.post("/", controller.createDoctor);
+
 router.get("/", controller.getAllDoctors);
+
+router.get("/:id", controller.getDoctor);
+
+router.put("/:id", controller.updateDoctor);
 
 module.exports = router;
