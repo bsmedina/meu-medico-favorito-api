3e98a9006687b5634281e889bf6d82ece64a7539
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-11-26 22:05:00 -0300

    Metodo para buscar um doctor e atualizar doctor

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -28,16 +28,79 @@ const getAllDoctors = async (req, res) => {
         }
 
     } catch (error) {
-        //res.status(500).send({ message: error.message })
-        messageError(res, error);
+        sendErrorMessage(error)
     }
 }
 
-const messageError = (res, error) => {
+const getDoctor = async (req, res) => {
+    const doctorId = req.params.id;
+
+    try {
+
+        const doctor = await Doctor.findOne({
+            where: { id: doctorId }
+        });
+    
+        if (doctor) {
+            res.status(200).send(doctor)
+        } else {
+            res.status(404).send({ message: `Médico de id ${doctorId} não foi encontrado na base.`})
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+};
+
+const updateDoctorInDB = async ({ name, specialty, phone, crm, favorite, doctorId, clinic }) => {
+    const rowsUpdated = await Doctor.update({ name, crm, specialty, clinic, phone, favorite }, {
+        where: { id: doctorId }
+    })
+        return (rowsUpdated && rowsUpdated[0] > 0) ? true : false
+}
+
+const updateDoctor = async (req, res) => {
+    const doctorId = req.params.id;
+    const { name, crm, specialty, clinic, phone, favorite } = req.body;
+
+    try {
+        const doctorUpdated = await updateDoctorInDB({ name, specialty, phone, crm, favorite, doctorId, clinic })
+
+        doctorUpdated
+        ? res.status(200).send({ message: "Medico alterado com sucesso" })
+        : res.status(404).send({ message: "Medico nao encontrado para alterar" })
+
+    } catch (error) {
+        sendErrorMessage(error)
+    }
+} 
+
+const sendErrorMessage = error => {
     res.status(500).send({ message: error.message })
 }
+
+/* const updateDoctor = async (req, res) => {
+    const doctorId = req.params.id;
+    const { name, crm, specialty, clinic, phone, favorite } = req.body;
+
+    try {
+        const rowsUpdated = await Doctor.update({name, crm, specialty, clinic, phone, favorite}, {
+            where: { id: doctorId }
+        });
+
+        if (rowsUpdated && rowsUpdated[0] > 0 ) {
+            res.status(200).send({ message: `Médico alterado com sucesso!`});
+        } else {
+            res.status(404).send({ message: `Médico não encontrado para alterar`});
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+
+} */
 
 module.exports = {
     createDoctor,
     getAllDoctors,
+    getDoctor,
+    updateDoctor
 }
